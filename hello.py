18f6ad9428ff38561d29148a28a45d71057a1b7a
mariasok2 <maria.sokolova@gmail.com>
2018-05-23 13:57:59 +1000

    Added hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+print ("Hello world!")
+
+a=2
+b=3
+sum=a+b
+print("sum=a+b")
+mylist=[a,b]
+print(mylist[0])
+
+def mariaf(horse):
+    print("Horses and wallabies. I am printing "+str(horse))
+
+mariaf(5)
